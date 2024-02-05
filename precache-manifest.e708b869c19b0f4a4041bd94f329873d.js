8549d0982565d0246e8dca38724627d91af2f3dc
Brijendra Tiwari <brijendratiwari@Brijendras-MacBook-Pro.local>
2024-02-05 16:43:29 +0530

    Updates

---
+++ precache-manifest.e708b869c19b0f4a4041bd94f329873d.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2fffb0814ade959a35cedf809c6fcc1",
+    "url": "/investfar-client/index.html"
+  },
+  {
+    "revision": "34c1e921cbf9c8029a39",
+    "url": "/investfar-client/static/css/2.c0dd2551.chunk.css"
+  },
+  {
+    "revision": "aa95fc42e7d42f9b9409",
+    "url": "/investfar-client/static/css/main.191b46ea.chunk.css"
+  },
+  {
+    "revision": "34c1e921cbf9c8029a39",
+    "url": "/investfar-client/static/js/2.07396749.chunk.js"
+  },
+  {
+    "revision": "a11c089898ca62613f2b462d9e336364",
+    "url": "/investfar-client/static/js/2.07396749.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa95fc42e7d42f9b9409",
+    "url": "/investfar-client/static/js/main.7fb3ba65.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/investfar-client/static/js/main.7fb3ba65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "495ec6dfeebfe862a0fe",
+    "url": "/investfar-client/static/js/runtime-main.6bfa9db3.js"
+  },
+  {
+    "revision": "8b8cb19e0aae57c206f4db262b301266",
+    "url": "/investfar-client/static/media/Mike-Myers.8b8cb19e.jpg"
+  },
+  {
+    "revision": "a6537905226044bd5878de832e9c0139",
+    "url": "/investfar-client/static/media/ac.a6537905.png"
+  },
+  {
+    "revision": "ff74d50c695c74a3181768b8bddd12ec",
+    "url": "/investfar-client/static/media/calculator.ff74d50c.png"
+  },
+  {
+    "revision": "1ae8d2e68b1619e8249b1b46a0d59b5d",
+    "url": "/investfar-client/static/media/cross.1ae8d2e6.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/investfar-client/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/investfar-client/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/investfar-client/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/investfar-client/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/investfar-client/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "88d8bed8cb39faee1362ca8cecdf88f5",
+    "url": "/investfar-client/static/media/graden.88d8bed8.png"
+  },
+  {
+    "revision": "8d17c9af5a5806309441262e7d48c567",
+    "url": "/investfar-client/static/media/image9.8d17c9af.png"
+  },
+  {
+    "revision": "a60462f86fd1a95f92f6f50848363e24",
+    "url": "/investfar-client/static/media/loginlogo.a60462f8.jpg"
+  },
+  {
+    "revision": "6d19b74e65fa8c2af2ba700b2713f120",
+    "url": "/investfar-client/static/media/walkscoure.6d19b74e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.b7ea3936219ea9b75c00860edd5403f0.js"
+  "/investfar-client/precache-manifest.e708b869c19b0f4a4041bd94f329873d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/investfar-client/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
