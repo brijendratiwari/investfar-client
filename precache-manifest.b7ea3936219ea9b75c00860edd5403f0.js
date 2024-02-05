06be70fa4ad5e597784da35dff6e39c76bd6c945
Brijendra Tiwari <brijendratiwari@Brijendras-MacBook-Pro.local>
2024-02-05 16:40:25 +0530

    Updates

---
+++ precache-manifest.b7ea3936219ea9b75c00860edd5403f0.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f36c39fed3d1c6c1d0c13c48270ce89d",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "29ac2165e7664b2cf847",
+    "url": "/my-app/static/css/2.10e4f7c6.chunk.css"
+  },
+  {
+    "revision": "aa95fc42e7d42f9b9409",
+    "url": "/my-app/static/css/main.191b46ea.chunk.css"
+  },
+  {
+    "revision": "29ac2165e7664b2cf847",
+    "url": "/my-app/static/js/2.07396749.chunk.js"
+  },
+  {
+    "revision": "a11c089898ca62613f2b462d9e336364",
+    "url": "/my-app/static/js/2.07396749.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa95fc42e7d42f9b9409",
+    "url": "/my-app/static/js/main.7fb3ba65.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/my-app/static/js/main.7fb3ba65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4c44597972658446371",
+    "url": "/my-app/static/js/runtime-main.5c9ca338.js"
+  },
+  {
+    "revision": "8b8cb19e0aae57c206f4db262b301266",
+    "url": "/my-app/static/media/Mike-Myers.8b8cb19e.jpg"
+  },
+  {
+    "revision": "a6537905226044bd5878de832e9c0139",
+    "url": "/my-app/static/media/ac.a6537905.png"
+  },
+  {
+    "revision": "ff74d50c695c74a3181768b8bddd12ec",
+    "url": "/my-app/static/media/calculator.ff74d50c.png"
+  },
+  {
+    "revision": "1ae8d2e68b1619e8249b1b46a0d59b5d",
+    "url": "/my-app/static/media/cross.1ae8d2e6.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/my-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/my-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/my-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/my-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/my-app/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "88d8bed8cb39faee1362ca8cecdf88f5",
+    "url": "/my-app/static/media/graden.88d8bed8.png"
+  },
+  {
+    "revision": "8d17c9af5a5806309441262e7d48c567",
+    "url": "/my-app/static/media/image9.8d17c9af.png"
+  },
+  {
+    "revision": "a60462f86fd1a95f92f6f50848363e24",
+    "url": "/my-app/static/media/loginlogo.a60462f8.jpg"
+  },
+  {
+    "revision": "6d19b74e65fa8c2af2ba700b2713f120",
+    "url": "/my-app/static/media/walkscoure.6d19b74e.png"
+  }
+]);
